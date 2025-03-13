62af74d6f35ac124fc9bad88ebcc50df7bc85ca5
A-lebe <alebachew.efrem2@gmail.com>
2025-03-13 12:12:10 -0500

    done

--- src/API/axios.js
+++ src/API/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // baseURL: "https://127.0.0.1:4002/clone-8e84f/us-central1/api",
-  baseURL:"http://127.0.0.1:5001/sep-449604/us-central1/api"
-  // baseURL:"http://localhost:2025"
+  // baseURL:"http://127.0.0.1:5001/sep-449604/us-central1/api"
+  baseURL:"http://localhost:5000"
 });
 
 export { axiosInstance };
